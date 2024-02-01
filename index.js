9de8b30254a14e6bfd038df62339e46252aada4a
Ray <xcz1207@gmail.com>
2024-01-31 21:24:18 -0800

    header links done!

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(express.static("public"));
 app.get("/", (req, res)=>{
     res.render("index.ejs");
 })
+app.get("/contact", (req, res)=>{
+    res.render("contact.ejs");
+})
+app.get("/about", (req, res)=>{
+    res.render("about.ejs");
+})
 
 app.listen(port, (req, res)=> {
     console.log(`Server is listening on port ${port}`);
